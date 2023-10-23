c22e573b072e0fd341e8d634fcda1bf064c89e77
DelfinaConca <delficonca@hotmail.com>
2023-10-23 14:45:30 -0300

    endpoint1

--- app.py
+++ app.py
@@ -2,15 +2,13 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-
+from cryptos import cryptos
 
 @app.route('/cryptos')
-def cryptos():
+def getcryptos():
     return jsonify({'cryptos': cryptos})
 
-@app.route('/cryptos')
-def cryptos():
-    return "Bienvenido a DonaOnline"
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
--- cryptos.py
+++ cryptos.py
@@ -1,3 +1,7 @@
-inicio = [
-    
-]
+cryptos = [
+    {"id":1 , "tipo":"Bitcoin" , "codigo": "BTC" },
+    {"id":2 , "tipo": "Ethereum" , "codigo": "ETH"},
+    {"id":3 , "tipo": "Cardano", "codigo": "ADA"},
+    {"id":4 , "tipo": "Tether" , "codigo":"USDT"},
+    {"id":5 , "tipo": "Dogecoin", "codigo":"DOGE"}
+    ]
