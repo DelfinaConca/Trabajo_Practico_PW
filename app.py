4f1173ab5c955eb8ec393c34d47fd39b7d6e8168
franciscocalle <panchocalle25@gmail.com>
2023-10-25 12:12:56 -0300

    mas endpoints

--- app.py
+++ app.py
@@ -1,13 +1,38 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
 from cryptos import cryptos
+from donaciones import donaciones
+
+@app.get("/")
+def home():
+    return render_template ("home.html")
 
 @app.route('/cryptos')
 def getcryptos():
     return jsonify({'cryptos': cryptos})
 
+
+@app.route('/donaciones', methods=['GET'])
+def getdonaciones():
+    return jsonify ({'donaciones': donaciones})
+
+@app.route('/form')
+def mostrar_formulario():
+    return render_template('formulario.html')
+
+# Endpoint para procesar la donación y agregarla al archivo donaciones.py
+@app.route('/donar', methods=['POST'])
+def donar():
+    criptomoneda = request.form.get('criptomoneda')
+    cantidad = request.form.get('cantidad')
+
+    # Agregar la donación al archivo donaciones.py
+    from donaciones import donaciones  # Importar la lista de donaciones desde donaciones.py
+    donaciones.append({"criptomoneda": criptomoneda, "cantidad": cantidad})
+
+    return "Donación exitosa"
 
 
 if __name__ == '__main__':
---
+++ products.py
@@ -0,0 +1,27 @@
+products = [
+    {
+        'name': 'Ropa',
+        'price': 10.0,
+        'quantity': 5
+    },
+    {
+        'name': 'Juguetes',
+        'price': 5.0,
+        'quantity': 10
+    },
+    {
+        'name': 'Libros',
+        'price': 7.5,
+        'quantity': 15
+    },
+    {
+        'name': 'Electrodomésticos',
+        'price': 50.0,
+        'quantity': 3
+    },
+    {
+        'name': 'Muebles',
+        'price': 30.0,
+        'quantity': 2
+    }
+]
