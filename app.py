2879284aaacd3cfa3a893e16bf91de525ef2fe7f
DelfinaConca <delficonca@hotmail.com>
2023-10-25 18:37:43 -0300

    cambios

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 
 from cryptos import cryptos
 from donaciones import donaciones
+from politicasyterminosdeuso import politicasyterminosdeuso, reglas
+from politicasdeprivacidad import politicasdeprivacidad, informacionquerecopilamos, usodelainformacion, compartirinformacion, seguridaddedatos, cookiesytecnologiassimilares, cambiosenlapoliticadeprivacidad, contacto
 
 @app.get("/")
 def home():
@@ -13,6 +15,14 @@ def home():
 def getcryptos():
     return jsonify({'cryptos': cryptos})
 
+@app.route('/politicas-y-terminos-de-uso')
+def getpoliticasyterminosdeuso():
+    return jsonify({'Politicas y Terminos de Uso': politicasyterminosdeuso, 'Reglas':reglas})
+    
+
+@app.route('/politicas-de-privacidad')
+def getpoliticasdeprivacidad():
+    return jsonify({ 'Politicas de Privacidad': politicasdeprivacidad,'Info. que recopilamos':informacionquerecopilamos,"Uso de la info.": usodelainformacion, "Compartir info.": compartirinformacion, "Seguridad de datos": seguridaddedatos, "Cookies": cookiesytecnologiassimilares, "Cambios en la politica de privacidad": cambiosenlapoliticadeprivacidad, "Contacto": contacto })
 
 @app.route('/donaciones', methods=['GET'])
 def getdonaciones():
---
+++ politicasdeprivacidad.py
@@ -0,0 +1,25 @@
+politicasdeprivacidad = [
+    {"Política de Privacidad de": "Dona Online","Última actualización": "25/10/2023"},
+    "Dona Online se compromete a proteger su privacidad y a respetar sus datos personales. Esta Política de Privacidad describe cómo recopilamos, utilizamos y compartimos información personal a través de nuestra plataforma."]
+
+informacionquerecopilamos=[
+    {"Información proporcionada por el usuario:": "ecopilamos información que usted proporciona al registrarse en la plataforma, hacer donaciones y utilizar nuestros servicios. Esto puede incluir su nombre, dirección de correo electrónico, información de contacto y preferencias de donación.","Información de transacciones":"Recopilamos información relacionada con las transacciones de donación, incluyendo detalles sobre las criptomonedas utilizadas, las causas apoyadas y las cantidades donadas.","Información de uso": "Recopilamos datos sobre cómo utiliza nuestra plataforma, como las páginas visitadas, el tiempo que pasa en el sitio y las acciones realizadas.", "Información de seguridad": " Podemos recopilar información de seguridad, como direcciones IP y datos de autenticación, para proteger nuestra plataforma y los usuarios de actividades maliciosas"}]
+
+usodelainformacion=[
+    "Utilizamos la información que recopilamos para gestionar las donaciones, proporcionar servicios personalizados, mejorar nuestra plataforma y comunicarnos con los donantes.Podemos utilizar su información para enviar actualizaciones, noticias, y oportunidades de donación relacionadas con Dona Online."]
+
+compartirinformacion=[
+    "No compartimos información personal con terceros sin su consentimiento, a menos que sea necesario para proporcionar los servicios que usted solicita o si estamos obligados a hacerlo por ley.Podemos compartir información agregada y anónima con terceros con fines estadísticos y de análisis."]
+
+seguridaddedatos=[
+    "Tomamos medidas de seguridad para proteger su información contra la pérdida, el acceso no autorizado y la alteración. Sin embargo, debido a la naturaleza de las criptomonedas, no podemos garantizar la seguridad completa de las transacciones."]
+
+cookiesytecnologiassimilares =[
+    "Dona Online puede utilizar cookies y tecnologías similares para mejorar la experiencia del usuario. Puede configurar su navegador para rechazar cookies, pero esto puede afectar la funcionalidad de la plataforma"]
+
+cambiosenlapoliticadeprivacidad=[
+    "Nos reservamos el derecho de modificar esta Política de Privacidad en cualquier momento. Las modificaciones se harán efectivas al ser publicadas en la plataforma"
+]
+
+contacto = [
+    "Si tiene preguntas o inquietudes sobre nuestra Política de Privacidad, por favor contáctenos a través de donaonline@gmail.com. Gracias por confiar en Dona Online para donar de manera segura y proteger su privacidad."]
